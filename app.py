576384ee117ed6ef1a70dd8850f2b511b913fe06
John Bartucz <jobartucz@isd535.org>
2022-02-21 21:47:16 -0600

    removed the bson stuff

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from re import S
 from uuid import uuid4
 from pymongo import MongoClient
 from dotenv import load_dotenv
-from bson.json_util import dumps
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
@@ -240,7 +239,12 @@ def post_command():
         return jsonify(newid(rj.get('nickname')))
 
     if command == "allstats":
-        return jsonify(dumps(list(info.find()), indent=2))
+        allstats = {}
+        for u in info.find():
+            allstats[u['userid']]['nickname'] = u['nickname']
+            allstats[u['userid']]['words'] = u['words']
+
+        return jsonify(allstats)
 
     # the rest of the commands all require a userid
     userid = rj.get('userid')
@@ -298,7 +302,7 @@ def index():
     homepage += "<h2>Commands</h2>\n"
     homepage += "<ul>\n"
     homepage += "<li><strong>newid</strong> takes an optional 'nickname' argument, returns a new unique 'userid'</li>\n"
-    homepage += "<li><strong>newword</strong> takes only a 'userid' and returns a unique 'wordid' to use when guessing</li>\n"
+    homepage += "<li><strong>newword</strong> takes only a valid 'userid' and returns a unique 'wordid' to use when guessing</li>\n"
     homepage += "<li><strong>guess</strong> takes a 'userid', a 'wordid', and a 'guess'. Returns a 5-digit 'result' where each digit is '1' - correct letter in correct position, '2' - correct letter in wrong position, '3' - letter not in word</li>\n"
     homepage += "<li><strong>setnickname</strong> takes a 'userid' and a 'nickname'</li>\n"
     homepage += "<li><strong>getmyids</strong> takes only a 'userid' argument and returns a list of all userids that have this nickname</li>\n"
