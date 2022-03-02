00181adc2a095fa2ce93dbd882f185925da8a1c9
John Bartucz <jobartucz@isd535.org>
2022-03-02 09:03:07 -0600

    trying mongo locally

--- app.py
+++ app.py
@@ -467,6 +467,7 @@ def index():
 
 loadredis()
 
+
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=False, port=5000)
